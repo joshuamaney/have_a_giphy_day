5e4c54aba5dadf95a6c7bbd59b70fac40f15f914
Joshua Maney <joshuaraymaney@gmail.com>
2021-03-06 14:10:33 -0500

    added js to display the word and definition to the page

--- Docs/script.js
+++ Docs/script.js
@@ -1,11 +1,7 @@
-// submit function
-// handleSubmit - prevent default
-
 // display the word and definition and display gif
 
-
-var wordFormEl = document.querySelector("#word-form")
-var resultTextEl = document.querySelector('#sec-0ce7');
+var wordFormEl = document.querySelector("#word-form");
+var wordDisplayDivEl = document.getElementById('word-display');
 
 function getWord(inputVal) {
     var searchTerm = "https://wordsapiv1.p.rapidapi.com/words/" + inputVal + "/definitions?rapidapi-key=7dc5b6a1dcmsh2b1d2a7d90d8d46p129d6fjsn403dc5af20f0";
@@ -17,18 +13,32 @@ function getWord(inputVal) {
         }
     }).then(response => {
         return response.json();
-    }).then(function(data) {
+    })
+    .then(function(data) {
         console.log(data);
-            for (var i = 0; i < data.length; i++) {
-              var listItem = document.createElement('h1');
-              listItem.textContent = data[i].html_url;
-              resultTextEl.appendChild(listItem);
+            for (var i = 0; i < data.definitions.length; i++) {
+              var wordItem = document.createElement('h1');
+              var definition = document.createElement('p');
+              wordItem.textContent = data.word;
+              definition.textContent = data.definitions[i].definition;
+              wordDisplayDivEl.append(wordItem);
+              wordDisplayDivEl.append(definition);
+            //     console.log("-----", data.word, data.definitions[i].definition)
+            //   var htmlEl = $(`
+            //   <div>
+            //     <h1>${data.word}</h1>
+            //     <p>${data.definitions[i].definition}</p>
+            //   </div>
+            //   `)
+
+            //   wordDisplayDivEl.append(htmlEl)  
+
             }
     })
     
     .catch(err => {
         console.error(err);
-    })
+    });
     
 }
 
